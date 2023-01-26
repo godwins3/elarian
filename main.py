4f03677a591a341141bcdbc56c621fc0649a1863
godwins3 <praisegodwins4@gmail.com>
2023-01-26 16:46:57 +0300

    adding import csv api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
 from pymongo import MongoClient
 from typing import List,Optional
 from pydantic import BaseModel
@@ -43,6 +44,12 @@ async def login(user: User):
     existing_user = db.users.find_one({"username": user.username})
     if existing_user is not None:
         return {"message": "valid username or password"}
+
+@app.post("/import/csv")
+async def import_csv(file: UploadFile = File(...)):
+    df = pd.read_csv(file.file)
+    return df.to_dict()
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=9000, reload=True)
